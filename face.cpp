431ce1c20db713f55f5ad55ac91e88bc19b8540b
Sharlene <sharlene.ndsilva@gmail.com>
2019-12-13 21:14:17 +0000

    face.cpp works for faces, iou stuff might need tweaking though

--- face.cpp
+++ face.cpp
@@ -18,8 +18,8 @@ using namespace std;
 using namespace cv;
 
 /* constants */
-// const std::string GTFILENAME = "gt_faces_data.txt";
-const std::string GTFILENAME = "gt_darts_data.txt";
+const std::string GTFILENAME = "gt_faces_data.txt";
+// const std::string GTFILENAME = "gt_darts_data.txt";
 
 /* struct that contains data OF an IMAge to draw bb */
 struct datastruct {
@@ -33,20 +33,23 @@ void drawGroundTruth( string fname, Mat frame);
 float calcIOU(string fname, int px, int py, int pw, int ph, int col);
 float calcTPR(float iou[], int index);
 float calcF1score(float iou[], int index, int noOfDetected);
+void doCalc( string fname, Mat frame, std::vector<Rect> faces );
 
 /** Global variables */
-// String cascade_name = "frontalface.xml";
-String cascade_name = "dartcascade/cascade.xml";
+String cascade_name = "frontalface.xml";
+// String cascade_name = "dartcascade/cascade.xml";
 CascadeClassifier cascade;
 datastruct gt[16][16];
-// int no_of_face[16] = {0, 0, 0, 0, 1, 11, 1, 1, 0, 1, 0, 1, 0, 1, 2, 0};
-int no_of_face[16] = {1,1,1,1,1,1,1,1,2,1,3,1,1,1,2,1};
+int no_of_faces[16] = {0, 0, 0, 0, 1, 11, 1, 1, 0, 1, 0, 1, 0, 1, 2, 0};
+// int no_of_face[16] = {1,1,1,1,1,1,1,1,2,1,3,1,1,1,2,1};
+
 
 /** @function main */
 int main( int argc, const char** argv ){
 	// get gt data from file
 	getGroundTruthData();
-	std::cout << "data read" << std::endl;
+	std::cout << " -> data read" << std::endl;
+
 	// 1. Read Input Image
 	Mat frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
 
@@ -55,14 +58,13 @@ int main( int argc, const char** argv ){
 
 	// ---> display ground truth boxes
 	drawGroundTruth(argv[1], frame);
-	std::cout << "ground truth drawn" << std::endl;
-
+	std::cout << " -> ground truth drawn" << std::endl;
 
 	// 3. Detect Faces and Display Result
 	detectAndDisplay( argv[1], frame );
 
 	// 4. Save Result Image
-	imwrite( "drawn.jpg", frame );
+	imwrite( "faceGT.jpg", frame );
 
 	return 0;
 }
@@ -81,22 +83,88 @@ void detectAndDisplay( string fname, Mat frame ){
 	cascade.detectMultiScale( frame_gray, faces, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
 
 	// 3. Print number of Faces found
-	std::cout << faces.size() << std::endl;
+	std::cout << "no. faces detected: " << faces.size() << std::endl;
 
 	// 4. Draw box around faces found
 	for( int i = 0; i < faces.size(); i++ ){
 		// print details of faces
-        	std::cout << "data of detected face #"<< i+1 << " is: " ;
-		cout<< faces[i].x << " " << faces[i].y << " " << faces[i].width << " " << faces[i].height << std::endl;
-
+		// std::cout << "data of detected face #"<< i+1 << " is: " ;
+		// cout<< faces[i].x << " " << faces[i].y << " " << faces[i].width << " " << faces[i].height << std::endl;
 		rectangle(frame, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar( 0, 255, 0 ), 2);
-
 	}
 
-	// IOU
-	// Plan: go though each ground truth and compare it to each face detecte
+	doCalc( fname, frame, faces);
+
+}
+
+// ----- SUBTASK 1 ----------------------------------------------------------------
+void getGroundTruthData(){
+	string line;
+	int oldIndex = 99;
+	int index;
+	string T;
+	int col = 0;
+
+	ifstream f;
+	f.open(GTFILENAME, ios::out);
 	
-	// get the number of faces 
+	// gets first line but we ignore it since its a comment
+	std::getline(f, line);
+	while(std::getline(f, line)){
+		// create a temp thing
+		std::stringstream temp(line);
+
+		// extract first value which is the index from line
+		getline(temp, T, ' ');
+		index = stoi(T);
+    	
+		if (oldIndex != index){
+			col = 0;
+		}
+
+		// extracting each value for x y w h as integer
+		getline(temp, T, ' ');
+		gt[index][col].x = stoi(T);
+		getline(temp, T, ' ');
+		gt[index][col].y = stoi(T);
+		getline(temp, T, ' ');
+		gt[index][col].w = stoi(T);
+		getline(temp, T, ' ');
+		gt[index][col].h = stoi(T);
+
+		if (index == 15){
+			break;
+		}
+		col++;
+		oldIndex = index;
+	}
+	f.close();
+}
+
+
+/* draws the ground truth (red) boxes */
+void drawGroundTruth(string fname, Mat frame){
+	int index = 0;
+	int col = 0;
+	// get image number ie index as in int
+	index = fname[4]-48;
+	char dot = '.';
+	if (fname[5] != dot){
+		index = stoi(to_string(index) + to_string(fname[5] - 48));
+	}
+
+	// draw rect
+	while(gt[index][col].x != 0){
+		rectangle(frame, Point(gt[index][col].x, gt[index][col].y), Point(gt[index][col].x + gt[index][col].w, gt[index][col].y + gt[index][col].h), Scalar( 0, 0, 255 ), 2);
+		if (gt[index][col].x == 0){
+			break;
+		}
+		col++;
+	}
+}
+
+
+void doCalc( string fname, Mat frame, std::vector<Rect> faces ){
 	int index = fname[4]-48;
 	char dot = '.';
 	if (fname[5] != dot){
@@ -104,29 +172,28 @@ void detectAndDisplay( string fname, Mat frame ){
 	}
 	// need variable to keep track of which one has been included
 	// lets use a new array of with size of the number of actualy faces
-	int facesMatched[no_of_face[index]]; 
+	int facesMatched[no_of_faces[index]]; 
 	
 	// NEED A LIST OF IOU TO PASS INTO TPR
-	float IOUs[no_of_face[index]];
+	float IOUs[no_of_faces[index]];
 
-	cout << "INDEX" << endl;
-	std::cout << index << std::endl;
+	cout << "-------------------"<<endl<< "INDEX: " << index << std::endl;
+	cout << "NO OF FACES actually: " << no_of_faces[index] << endl;
 	// for each ground truth face we perform IOU with all detected faces
-	for (int j = 0; j < no_of_face[index]; j++){
+	for (int j = 0; j < no_of_faces[index]; j++){
 		float IOU = 0;
 		int IOUIndex;
 		float tempIOU;
 
-		// for each detected face 
-		// store the largest one
+		// for each detected face store the largest one
 		for (int k = 0; k < faces.size(); k++){
 			// perform IOU and compare to previous and store the larger one	
-			
 			tempIOU = calcIOU(fname, faces[k].x, faces[k].y, faces[k].width, faces[k].height, j);
-			// std::cout << "temp: " << tempIOU << std::endl;
-			// if ((IOU < tempIOU) && (tempIOU < 1)){
-			 if (IOU < tempIOU){
-
+			// cout << "x: "<< faces[k].x << endl;
+			// cout << "y: "<<  faces[k].y<< endl; 
+			// cout << "width: " << faces[k].width << endl;
+			// cout << "height: " << faces[k].height << endl;
+			if (IOU < tempIOU){
 				if (j == 0){
 					IOU = tempIOU;
 				}
@@ -148,97 +215,23 @@ void detectAndDisplay( string fname, Mat frame ){
 		// store final one in facesMatched
 		facesMatched[j] = IOUIndex;
 		IOUs[j] = IOU;
-		// display the grround truth face we're currently at
-		std::cout << "gt face: " << j << std::endl;
 		// display the best IOU
 		std::cout << "iou: " << IOU << std::endl;
 	}
-	std::cout << "TPR: " << std::endl;
+
+	// TPR
 	float tpr = calcTPR(IOUs, index);
-	std::cout << tpr << std::endl;
+	std::cout << "----- TPR: " << tpr << std::endl;
 
 	// F1
+	// no of detected should increment whenever one we dont include
 	int noOfDetected = faces.size();
-	std::cout << "F1: " << std::endl; 
-	std::cout << calcF1score(IOUs, index, noOfDetected) << std::endl;
-}
+	cout << "detected: " << noOfDetected << endl;
+	calcF1score(IOUs, index, noOfDetected);
 
-// ----- SUBTASK 1 ----------------------------------------------------------------
-
-void getGroundTruthData(){
-	std::cout << "in GT" << std::endl;
-	
-	string line;
-	int oldIndex = 99;
-	int index;
-	string T;
-	int col = 0;
-
-	ifstream f;
-	f.open(GTFILENAME, ios::out);
-	
-	// gets first line but we ignore it since its a comment
-	std::getline(f, line);
-			while(std::getline(f, line)){
-		// create a temp thing
-		std::stringstream temp(line);
-
-		// extract first value which is the index from line
-		getline(temp, T, ' ');
-		index = stoi(T);
-
-		std::cout << index << std::endl;
-    	
-		if (oldIndex != index){
-			col = 0;
-		}
-
-		// extracting each value for x y w h as integer
-		getline(temp, T, ' ');
-		gt[index][col].x = stoi(T);
-		getline(temp, T, ' ');
-		gt[index][col].y = stoi(T);
-		getline(temp, T, ' ');
-		gt[index][col].w = stoi(T);
-		getline(temp, T, ' ');
-		gt[index][col].h = stoi(T);
-		//std::cout << "end of one line" << std::endl;
-		if (index == 15){
-			break;
-		}
-		cout<< gt[index][col].x << " " << gt[index][col].y << " " <<gt[index][col].w << " "<<gt[index][col].w << endl;
-		col++;
-		oldIndex = index;
-	}
-	f.close();
 }
 
 
-/* draws the ground truth (red) boxes */
-void drawGroundTruth(string fname, Mat frame){
-	int index = 0;
-	int col = 0;
-	// get image number ie index as in int
-	index = fname[4]-48;
-	char dot = '.';
-	if (fname[5] != dot){
-		index = stoi(to_string(index) + to_string(fname[5] - 48));
-	}
-	for (int i = 0 ; i < 15; i++) {
-		cout<< gt[index][i].x << endl;
-	}
-	// draw rect
-	while(gt[index][col].x != 0){
-		std::cout << col << std::endl;		
-
-		rectangle(frame, Point(gt[index][col].x, gt[index][col].y), Point(gt[index][col].x + gt[index][col].w, gt[index][col].y + gt[index][col].h), Scalar( 0, 0, 255 ), 2);
-		if (gt[index][col].x == 0){
-			break;
-		}
-		col++;
-		std::cout << gt[index][col].x << std::endl;	
-	}
-}
 
 /*
 this diagram is useful for iou calculation below
@@ -255,6 +248,7 @@ px, py, pw, ph -> predicted data
 gx, gy, gw, gh -> ground truth data
 */
 float calcIOU(string fname, int px, int py, int pw, int ph, int col){
+	cout << "COLE: "<< col << endl;
 	// - get index & declare variables
 	int index = fname[4]-48;
 	char dot = '.';
@@ -284,7 +278,7 @@ float calcIOU(string fname, int px, int py, int pw, int ph, int col){
 	iy2 = max(gy, py);
 
 	// - calc the area of intersection
-	if (((ix1 - ix2) > 0) && ((iy1 - iy2) > 1)){
+	if (((ix1 - ix2) > 0) && ((iy1 - iy2) > 0)){
 		intersect_area = abs(ix1 - ix2) * abs(iy1 - iy2);		
 	}
 	else return 0;
@@ -296,56 +290,53 @@ float calcIOU(string fname, int px, int py, int pw, int ph, int col){
 	return iou;
 }
 
-// True P Rate is the fraction of successfully detected faces out of all valid faces in an image
+
 float calcTPR(float iou[], int index){
 	int TP = 0;
-
 	// for each IOU value
-	for (int i = 0 ; i < no_of_face[index]; i++){
+	for (int i = 0 ; i < no_of_faces[index]; i++){
 		if (iou[i] >= 0.5 ){
 			// then it is a true positivei
 			TP = TP + 1;
 		}
 	}
-	std::cout << TP << std::endl;
-	std::cout << no_of_face[index] << std::endl;
-	float tpr = (float) TP / no_of_face[index];
-	std::cout << tpr << std::endl;
+	float tpr = (float) TP / no_of_faces[index];
 	return tpr;
 }
 
-/*
-	formula is: 2 * ([precision * recall]/[precision + recall])
-	precision = TP / (TP + FP)
-	recall = TP / (TP + FN)
 
-	so we need the TP, FN, FP and FN
-	// TP(TPCount)
-		// Those detected that are actually detected
-
-	// FP(no detected - no of actual) -> no faces but some are detected 
-		// Those not supposed to be detected but is detected
-		// no of faces detected- actual number 
-
-	// FN(no of faces - no detected) -> there are faces but not detected
-		// Those should be detected but not
-		// if IOU == 0
-*/
 float calcF1score(float iou[], int index, int noOfDetected){
-	int TP, FN = 0;
-	int FP = noOfDetected - no_of_face[index];
-	cout << "index" << index << endl;
-	for (int i = 0 ; i < no_of_face[index]; i++){
+	int TP = 0;
+	int FN = 0;
+	int FP = noOfDetected - no_of_faces[index];
+
+	if (FP < 0){
+		FP = 0;
+	}
+	cout << endl;
+	cout << "index: " << index << endl;
+	for (int i = 0 ; i < no_of_faces[index]; i++){
+	cout << "IOU VALUE: "<< iou[i] << endl;
 		if (iou[i] >= 0.5 ){
 			// then it is a true positive
 			TP = TP + 1;
+		}
 		if (iou[i] == 0) {
-			FN = FN + 1;}
+			FN = FN + 1;
 		}	
 	}
+	cout << "fp: " << FP << endl;
+	cout << "tp: " << TP << endl;
+	cout << "fn: " << FN << endl;
 	// ACTUAL F1	
 	float precision = (float) TP / (TP + FP);
 	float recall = (float) TP / (TP + FN);
+	cout<< "precision: " << precision << endl;
+	cout << "recall: " << recall << endl;
 
-	return 2 * ((precision * recall)/(precision + recall));
+	float f1; 
+	f1 = 2 * ((precision * recall)/(precision + recall));
+
+	cout<< "------ F1 SCORE: " <<  f1 << endl;;
+	return f1;
 }
